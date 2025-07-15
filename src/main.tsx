e8f0286f98e38ceca2e3fbb9283165b04c6d59b5
shj <to@shj.rip>
2025-07-15 22:44:12 +0900

    feat: use StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { App } from './app.tsx';
 import './i18n';
 import '@unocss/reset/tailwind.css';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'virtual:uno.css';
 
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
